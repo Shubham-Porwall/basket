ee821643d6f03b9fb8ba2ca703355ec02a10be11
Bhagwan Singh Rajput <bhagwanrajputror963@gmail.com>
2025-11-12 15:02:39 +0530

    feat: initiaze Basket class with product list

---
+++ basket.rb
@@ -0,0 +1,11 @@
+class Basket
+  PRODUCTS = {
+    'R01' => { name: 'Red Widget', price: 32.95 },
+    'G01' => { name: 'Green Widget', price: 24.95 },
+    'B01' => { name: 'Blue Widget', price: 7.95 }
+  }.freeze
+
+  def initialize
+    @items = []
+  end
+end
